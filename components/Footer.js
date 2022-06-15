fb26ecd5a124f125a9f7ee81fcee906f3ba5e304
Alex <77445616+AlexCao97@users.noreply.github.com>
2022-06-15 17:21:36 +0700

    component Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,46 @@
 import React from "react";
 import styles from "@/styles/Footer.module.css";
 import Avatar from "@mui/material/Avatar";
+import Image from "next/image";
+import Facebook from "@/assets/icons/facebook.png";
+import Instagram from "@/assets/icons/instagram.png";
+import Linkedin from "@/assets/icons/linkedin.png";
+import Twitter from "@/assets/icons/twitter.png";
+import Youtube from "@/assets/icons/youtube.png";
 
 export default function Footer() {
   return (
-    <div>
-      <div className={styles.footer}>
+    <div className={styles.footer}>
+      <div className={styles.avatar}>
         <span className={styles.line} />
         <Avatar
           sx={{
             marginRight: "24px",
             marginLeft: "24px",
-            width: "72px",
-            height: "72px",
+            width: "55px",
+            height: "55px",
           }}
-          alt="Cao Nguyễn Vũ Toàn"
           src="https://scontent-frt3-1.xx.fbcdn.net/v/t39.30808-1/287944488_3109254572738360_6676872013096327565_n.jpg?stp=dst-jpg_p160x160&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_ohc=Dcf_QcW0nBAAX-NivZa&_nc_ht=scontent-frt3-1.xx&oh=00_AT82dFndJVEkcbXr6HDr3xGaowpbyG3sfUkYbSi4ESMIBw&oe=62AF3030"
         />
         <span className={styles.line} />
       </div>
-      <div></div>
+      <div className={styles.socialContainer}>
+        <div className={styles.socialLogo}>
+          <Image src={Facebook} alt="Facebook" />
+        </div>
+        <div className={styles.socialLogo}>
+          <Image src={Instagram} alt="Instagram" />
+        </div>
+        <div className={styles.socialLogo}>
+          <Image src={Linkedin} alt="Linkedin" />
+        </div>
+        <div className={styles.socialLogo}>
+          <Image src={Twitter} alt="Twitter" />
+        </div>
+        <div className={styles.socialLogo}>
+          <Image src={Youtube} alt="Youtube" />
+        </div>
+      </div>
     </div>
   );
 }
