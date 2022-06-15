42d8965aad69cbcc4218e8bc28313f5dbe11eea9
Alex <77445616+AlexCao97@users.noreply.github.com>
2022-06-15 17:21:44 +0700

    Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -22,21 +22,6 @@ export default function BasicAppBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, fontWeight: "bold" }}
-          >
-            CURRICULUM VITAE
-          </Typography>
-          <Tooltip title="Profile">
-            <IconButton sx={{ p: 0 }}>
-              <Avatar
-                alt="Cao Nguyễn Vũ Toàn"
-                src="https://scontent-frt3-1.xx.fbcdn.net/v/t39.30808-1/287944488_3109254572738360_6676872013096327565_n.jpg?stp=dst-jpg_p160x160&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_ohc=Dcf_QcW0nBAAX-NivZa&_nc_ht=scontent-frt3-1.xx&oh=00_AT82dFndJVEkcbXr6HDr3xGaowpbyG3sfUkYbSi4ESMIBw&oe=62AF3030"
-              />
-            </IconButton>
-          </Tooltip>
         </Toolbar>
       </AppBar>
     </Box>
